8600c4a32198d0ba76e7b982c9b8359195f7355e
Jeremy <gojerjeremy@gmail.com>
2023-05-12 10:27:00 +0530

    Chat

--- public/scripts/messenger.js
+++ public/scripts/messenger.js
@@ -3,9 +3,12 @@ $.ajax({
     url: "https://localhost:9000/getall",
     type: "GET",
     success: (req,res)=>{
-        console.log(req.body);
+        console.log(req);
         console.log(res);
-        var arr = req.body;
+        var arr = req;
+        for(m of arr){
+            
+        }
     },
     error:()=>{
         console.log("there was a error with ajax");
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import { fileURLToPath } from 'url'
 import path , { dirname } from 'path'
+import bodyParser from 'body-parser';
 const __filename = fileURLToPath(import.meta.url)
 
 const _dir = path.join(dirname(__filename),'public')
 var app = express();
-var msqq = ['Hello','ahchj','hjvhasdv','sagdvhsad']
+var msqq = []
 
 app.use(express.static(_dir))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(_dir,'welcome.html'));
@@ -21,7 +24,7 @@ app.get('/getall',(req,res)=>{
     res.send(msqq);
 })
 app.post('/send',(req,res)=>{
-    msqq.push(req.body);
+    msqq.push(req.body.msg);
     res.redirect('/msngr');
 })
 
